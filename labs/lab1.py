95943af76303c8b38e1a5d3c14b53d96ab251dc1
scamer22 <144202121+scamer22@users.noreply.github.com>
2023-09-27 10:58:18 +0500

    Update lab1.py

--- labs/lab1.py
+++ labs/lab1.py
@@ -1,54 +1,127 @@
-#Задание 1
-#from math import *
+def task1():
+    from math import *
 
-x = int(input())
-y = int(input())
-z = int(input())
+    x = int(input())
+    y = int(input())
+    z = int(input())
 
-l = log10(1 + x**2) - abs(y**(1/3))
-r = x ** (1/3) + y**(1/4)
-a = l / r
-b = z**(1/2) + y**2 + log(x)
+    l = log10(1 + x ** 2) - abs(y ** (1 / 3))
+    r = x ** (1 / 3) + y ** (1 / 4)
+    a = l / r
+    b = z ** (1 / 2) + y ** 2 + log(x)
 
-print(a, b)
+    print(a, b)
 
-#задание 2
-#from math import *
+task1()
 
-a = 1
-b = 3
-c = -4
+def task2():
+ from math import *
 
-def f(x):
-    return x**b + ((b*x)**(1/2)) / c*x+a
+ a = 1
+ b = 3
+ c = -4
 
-print(f(x))
+ def f(x):
+     return x ** b + ((b * x) ** (1 / 2)) / c * x + a
 
-#Задание 3
-from math import *
+ print(f(x))
+task2()
 
-def f(x):
-    return 3**(-2*x) - ((cos(x) + sin(3*x))**(1/3))
-print(f(x))
+def task3():
+ from math import *
 
-#Задание 4
-from math import *
+ def f(x):
+     return 3 ** (-2 * x) - ((cos(x) + sin(3 * x)) ** (1 / 3))
 
-r = int(input())
-h = int(input())
+ print(f(x))
+task3()
 
-v = 1/3 * pi*(r ** 2) * h
-l = r**2 + h**2
+def task4():
+ from math import *
 
-print(l)
+ r = int(input())
+ h = int(input())
 
-#Задание 5
-v1 = float(input())
-v2 = float(input())
-h = float(input())
+ v = 1 / 3 * pi * (r ** 2) * h
+ l = r ** 2 + h ** 2
 
-time = h / (v1 - v2)
-distance = v1 * time
+ print(l)
+task4()
 
-print(time)
-print(distance)
+def task5():
+ v1 = float(input())
+ v2 = float(input())
+ h = float(input())
+
+ time = h / (v1 - v2)
+ distance = v1 * time
+
+ print(time)
+ print(distance)
+
+task5()
+
+def task6():
+ import math
+
+ def f(l, a):
+     h = math.sqrt(l ** 2 - a ** 2)
+     return h
+
+ l = float(input())
+ a = float(input())
+ h = f(l, a)
+
+ print(h)
+
+task6()
+
+def task7():
+ import math
+
+ A = float(input())
+ B = float(input())
+ C = float(input())
+
+ D = B ** 2 - 4 * A * C
+
+ if D >= 0:
+     x1 = (-B + math.sqrt(D)) / (2 * A)
+     x2 = (-B - math.sqrt(D)) / (2 * A)
+
+     print(x1)
+     print(x2)
+ else:
+     print('aye')
+
+task7()
+
+def task8():
+ def f(v, a):
+     t = (2 * v) / a
+     s = (a * (t ** 2)) / 2
+     return s
+
+ v = float(input())
+ a = float(input())
+ l = f(v, a)
+
+ print(l)
+
+task8()
+
+def task9():
+    
+    def f(value, total):
+        percentage = (value / total) * 100
+        if percentage > 5:
+            return percentage
+        else:
+            return 5
+    
+    print(f())
+
+task9()
+
+
+
